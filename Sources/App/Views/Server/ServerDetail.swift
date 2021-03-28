d3fc86aa8628ba533ed16ffc7211b0324f13f3f9
Fabrizio Brancati <fabrizio.brancati@wisemotions.com>
2021-03-28 20:56:45 +0200

    Add ServerDetail View

---
+++ Sources/App/Views/Server/ServerDetail.swift
@@ -0,0 +1,11 @@
+import Foundation
+import SwiftUI
+
+struct ServerDetail: View {
+  var body: some View {
+    VStack {
+      Text("Detail")
+    }
+    .frame(minWidth: Constants.minimumDetailWidth)
+  }
+}
