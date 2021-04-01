afc36f38118ba46ab86ccc255c714ef0b928fae1
Fabrizio Brancati <fabrizio.brancati@wisemotions.com>
2021-04-01 21:24:27 +0200

    Add public init functions to NetworkExchange

--- Sources/Server/Models/NetworkExchange.swift
+++ Sources/Server/Models/NetworkExchange.swift
@@ -8,6 +8,15 @@ public struct NetworkExchange {
 
   /// The object containing the details about the sent response.
   public let response: DetailedResponse
+
+  /// Creates a `NetworkExchange` object.
+  /// - Parameters:
+  ///   - request: The object containing the details about the received request.
+  ///   - response: The object containing the details about the sent response.
+  public init(request: DetailedRequest, response: DetailedResponse) {
+    self.request = request
+    self.response = response
+  }
 }
 
 /// An object containing the details about the received request.
@@ -28,6 +37,19 @@ public struct DetailedRequest {
   public var queryItems: [URLQueryItem] {
     URLComponents(string: uri.string)?.queryItems ?? []
   }
+
+  /// Creates a `DetailedRequest` object.
+  /// - Parameters:
+  ///   - httpMethod: The `HTTP` method associated with the request.
+  ///   - uri: The `URI` that invoked the start of the exchange.
+  ///   - headers: The headers associated with the request.
+  ///   - timestamp: The timestamp of when the request was invoked.
+  public init(httpMethod: HTTPMethod, uri: URI, headers: HTTPHeaders, timestamp: TimeInterval) {
+    self.httpMethod = httpMethod
+    self.uri = uri
+    self.headers = headers
+    self.timestamp = timestamp
+  }
 }
 
 /// An object containing the details about the sent response.
@@ -47,8 +69,23 @@ public struct DetailedResponse {
   ///
   /// - Access `.code` for the numerical output.
   /// - Access `.reasonPhrase` for the phrasal output.
-  public let responseStatus: HTTPResponseStatus
+  public let status: HTTPResponseStatus
 
   /// The timestamp of when the response was ready.
   public let timestamp: TimeInterval
+
+  /// Creates a `DetailedResponse` object.
+  /// - Parameters:
+  ///   - httpMethod: The `HTTP` method that prompted the response.
+  ///   - uri: The `URI` considered when answering to the started exchange.
+  ///   - headers: The headers associated with the response.
+  ///   - status: The `HTTP` response status code.
+  ///   - timestamp: The timestamp of when the response was ready.
+  public init(httpMethod: HTTPMethod, uri: URI, headers: HTTPHeaders, status: HTTPResponseStatus, timestamp: TimeInterval) {
+    self.httpMethod = httpMethod
+    self.uri = uri
+    self.headers = headers
+    self.status = status
+    self.timestamp = timestamp
+  }
 }
