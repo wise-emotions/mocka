12d86f77cb73ddd3c8c6a47b74963c06d618210a
TheInkedEngineer <firas@hey.com>
2021-04-04 08:19:48 +0200

    Add ServerTests main class

---
+++ Tests/ServerTests/ServerTests.swift
@@ -0,0 +1,46 @@
+//
+// Mocka
+//
+
+import XCTest
+
+@testable import MockaServer
+
+class ServerTests {}
+
+// MARK: - Data Structures
+
+extension ServerTests {
+  struct ServerConfiguration: ServerConfigurationProvider {
+    var requests: Set<Request> = [
+      Request(
+        method: .get,
+        path: ["api", "test"],
+        requestedResponse: RequestedResponse(
+          status: .ok,
+          headers: ["Content-Type": "application/json"],
+          body: ResponseBody(
+            contentType: .applicationJSON,
+            fileLocation: ServerTests.getFilePath(for: "DummyJSON", extension: "json")
+          )
+        )
+      )
+    ]
+  }
+}
+
+
+// MARK: - Helpers
+
+extension Bundle {
+  internal static var tests: Bundle {
+    Bundle(for: ServerTests.self)
+  }
+}
+
+extension ServerTests {
+  /// Returns the `URL` of the passed file in the `Bundle.tests`.
+  static func getFilePath(for name: String, `extension`: String) -> URL {
+    URL(string: Bundle.tests.path(forResource: name, ofType: `extension`)!)!
+  }
+}
