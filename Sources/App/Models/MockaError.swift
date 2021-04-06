cea131533d3117bf332bcc849e055eca216050e3
TheInkedEngineer <firas@hey.com>
2021-04-06 23:13:42 +0200

    Rename all rootPath in workspacePath

--- Sources/App/Models/MockaError.swift
+++ Sources/App/Models/MockaError.swift
@@ -7,16 +7,16 @@ import Foundation
 /// A list of errors that can be thrown inside of `Mocka` app.
 enum MockaError: Error {
   /// The root path value is missing when it's expected to be populated.
-  case missingRootPathValue
+  case missingWorkspacePathValue
 
   /// The root path does not exist.
-  case rootPathDoesNotExist
+  case workspacePathDoesNotExist
 
   /// The root path does not point to a folder.
-  case rootPathNotFolder
+  case workspacePathNotFolder
 
   /// The root path does not contain scheme.
-  case rootPathMissingScheme
+  case workspacePathMissingScheme
 
   /// An error occurred when trying to encode a file.
   case failedToEncode
