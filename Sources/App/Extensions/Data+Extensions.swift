79ab58bb59e38b857288e790bf6b2a9c4efb66d3
Fabrizio Brancati <fabrizio.brancati@wisemotions.com>
2021-04-09 22:06:14 +0200

    Remove `.sortedKeys` on json formatter

--- Sources/App/Extensions/Data+Extensions.swift
+++ Sources/App/Extensions/Data+Extensions.swift
@@ -8,7 +8,7 @@ extension Data {
   var asPrettyPrintedJSON: String? {
     guard
       let jsonObject = try? JSONSerialization.jsonObject(with: self, options: []),
-      let jsonData = try? JSONSerialization.data(withJSONObject: jsonObject, options: [.prettyPrinted, .sortedKeys]),
+      let jsonData = try? JSONSerialization.data(withJSONObject: jsonObject, options: [.prettyPrinted]),
       let prettyPrintedString = String(data: jsonData, encoding:.utf8)
     else {
       return nil
