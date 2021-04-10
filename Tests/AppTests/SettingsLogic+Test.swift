064db5795acd5d788f3d4f1d8858eb354a6bfb7c
TheInkedEngineer <firas@hey.com>
2021-04-10 10:37:36 +0200

    Add Settings serverConfiguration file tests

---
+++ Tests/AppTests/SettingsLogic+Test.swift
@@ -0,0 +1,51 @@
+//
+//  Mocka
+//
+
+import XCTest
+
+@testable import MockaApp
+
+class SettingsLogicTests: XCTestCase {
+  // The `URL` of a temporary folder we will use for this test.
+  static var temporaryWorkspaceURL: URL!
+
+  // The name of the configuration file that will be created.
+  private let configurationFileName = "serverConfiguration.json"
+
+  // The full path where the configuration file should be present.
+  private var configurationFilePath: String {
+    Self.temporaryWorkspaceURL.appendingPathComponent(configurationFileName).path
+  }
+
+  override class func setUp() {
+    temporaryWorkspaceURL = URL(fileURLWithPath: NSTemporaryDirectory().appending("Mocka"))
+    try? FileManager.default.createDirectory(atPath: Self.temporaryWorkspaceURL.path, withIntermediateDirectories: true, attributes: nil)
+  }
+
+  override class func tearDown() {
+    try? FileManager.default.removeItem(at: temporaryWorkspaceURL)
+    UserDefaults.standard.set(nil, forKey: UserDefaultKey.workspaceURL)
+  }
+
+  // Test that the creation of the server configuration file occurs at the root path.
+  func testSettingsFileCreationAtWorkspaceRoot() {
+    XCTAssertFalse(FileManager.default.fileExists(atPath: configurationFileName))
+    UserDefaults.standard.set(Self.temporaryWorkspaceURL, forKey: UserDefaultKey.workspaceURL)
+    try? Logic.Settings.createConfiguration()
+    XCTAssertTrue(FileManager.default.fileExists(atPath: configurationFilePath))
+  }
+
+  // Test that the creation of the server configuration file throws MockaError.workspacePathDoesNotExist if workspace URL is not set.
+  func testSettingsFileCreationFailsWithoutWorkspaceRoot() {
+    UserDefaults.standard.set(nil, forKey: UserDefaultKey.workspaceURL)
+    do {
+      try Logic.Settings.createConfiguration()
+    } catch {
+      guard case .workspacePathDoesNotExist = error as? MockaError else {
+        XCTFail("Expected a `MockaError.workspacePathDoesNotExist` but got \(error) instead")
+        return
+      }
+    }
+  }
+}
