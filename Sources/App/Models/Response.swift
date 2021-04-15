532caa0045a76b54d13e5c141bcfa39775bbd3f8
TheInkedEngineer <firas@hey.com>
2021-04-15 20:36:25 +0200

    Fix custom array of HTTPHeader coding

--- Sources/App/Models/Response.swift
+++ Sources/App/Models/Response.swift
@@ -6,7 +6,7 @@ import Foundation
 import MockaServer
 
 /// An object containing information regarding the response for a server request.
-struct Response: Codable, Hashable {
+struct Response: Hashable {
   /// The `HTTP` response status code.
   let statusCode: Int
 
@@ -42,5 +42,41 @@ struct Response: Codable, Hashable {
     }
 
     self.fileName = "response.\(fileExtension)"
+  }
+}
+
+// MARK: - Custom Coding
+
+extension Response: Codable {
+  enum CodingKeys: String, CodingKey {
+    case statusCode
+    case contentType
+    case headers
+    case fileName
+  }
+
+  func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: CodingKeys.self)
+
+    try container.encode(statusCode, forKey: .statusCode)
+    try container.encode(contentType, forKey: .contentType)
+    try container.encode(fileName, forKey: .fileName)
+
+    var headersDictionary: [String: String] = [:]
+    headers.forEach { headersDictionary[$0.key] = $0.value }
+    try container.encode(headersDictionary, forKey: .headers)
+  }
+
+  init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+
+    statusCode = try container.decode(Int.self, forKey: .statusCode)
+    contentType = try container.decode(ResponseBody.ContentType.self, forKey: .contentType)
+    fileName = try container.decodeIfPresent(String.self, forKey: .fileName)
+
+    let decodedHeaders = try container.decode([String: String].self, forKey: .headers)
+    headers = decodedHeaders.reduce(into: [HTTPHeader]()) {
+      $0.append(HTTPHeader(key: $1.key, value: $1.value))
+    }
   }
 }
