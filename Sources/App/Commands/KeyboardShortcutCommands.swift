9c7e754f829237fcdd4aae84b659b05a2a8a2afc
Michele <michele.mola@wisemotions.com>
2021-04-30 09:24:30 +0200

    Added commands

--- Sources/App/Commands/KeyboardShortcutCommands.swift
+++ Sources/App/Commands/KeyboardShortcutCommands.swift
@@ -9,10 +9,51 @@ struct KeyboardShortcutCommands: Commands {
 
   var body: some Commands {
     CommandGroup(replacing: .newItem) {
-      Button("Add new API") {
+      Button("New API") {
         appEnvironment.selectedSection = .editor
+
       }
       .keyboardShortcut("n")
+      
+      Button("Run") {
+        appEnvironment.selectedSection = .server
+        
+        guard let serverConfiguration = appEnvironment.serverConfiguration else {
+          appEnvironment.shouldShowStartupSettings = true
+          return
+        }
+
+        try? appEnvironment.server.start(with: serverConfiguration)
+        
+        appEnvironment.isServerRunning = true
+      }
+      .keyboardShortcut("r")
+      
+      Button("Stop") {
+        appEnvironment.selectedSection = .server
+        
+        try? appEnvironment.server.stop()
+        
+        appEnvironment.isServerRunning = false
+      }
+      .keyboardShortcut(".")
+    }
+    
+    CommandGroup(after: .windowArrangement) {
+      Button("Server") {
+        appEnvironment.selectedSection = .server
+      }
+      .keyboardShortcut("1")
+      
+      Button("Editor") {
+        appEnvironment.selectedSection = .editor
+      }
+      .keyboardShortcut("2")
+      
+      Button("Console") {
+        appEnvironment.selectedSection = .console
+      }
+      .keyboardShortcut("3")
     }
   }
 }
