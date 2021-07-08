912aee39f689c51d61e6e8d365c04765a13a3921
MicheleMola <MicheleMola@users.noreply.github.com>
2021-07-08 14:58:14 +0000

    Format code

--- Sources/App/Commands/KeyboardShortcutCommands.swift
+++ Sources/App/Commands/KeyboardShortcutCommands.swift
@@ -9,30 +9,30 @@ struct KeyboardShortcutCommands: Commands {
   @ObservedObject var appEnvironment: AppEnvironment
 
   var body: some Commands {
-    CommandGroup(replacing: .newItem) {      
+    CommandGroup(replacing: .newItem) {
       Button("Run") {
         appEnvironment.selectedSection = .server
-        
+
         guard let serverConfiguration = appEnvironment.serverConfiguration else {
           appEnvironment.shouldShowStartupSettings = true
           return
         }
 
         try? appEnvironment.server.start(with: serverConfiguration)
-        
+
         appEnvironment.isServerRunning = true
       }
       .keyboardShortcut("r")
-      
+
       Button("Stop") {
         appEnvironment.selectedSection = .server
-        
+
         try? appEnvironment.server.stop()
-        
+
         appEnvironment.isServerRunning = false
       }
       .keyboardShortcut(".")
-      
+
       Button("Restart") {
         guard let serverConfiguration = appEnvironment.serverConfiguration else {
           appEnvironment.shouldShowStartupSettings = true
@@ -43,18 +43,18 @@ struct KeyboardShortcutCommands: Commands {
       }
       .keyboardShortcut("R")
     }
-    
+
     CommandGroup(after: .windowArrangement) {
       Button("Server") {
         appEnvironment.selectedSection = .server
       }
       .keyboardShortcut("1")
-      
+
       Button("Editor") {
         appEnvironment.selectedSection = .editor
       }
       .keyboardShortcut("2")
-      
+
       Button("Console") {
         appEnvironment.selectedSection = .console
       }
