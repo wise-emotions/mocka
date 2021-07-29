25f35c58dc36202a4078cb66c023aac8953baea1
martin-e91 <martin.essuman@gmail.com>
2021-07-29 15:58:11 +0200

    Add doc

--- Sources/App/Models/InAppNotification.swift
+++ Sources/App/Models/InAppNotification.swift
@@ -5,11 +5,12 @@
 import MockaServer
 import UserNotifications
 
-/// An in app notification to be showed to the user.
+/// An in-app notification to be showed to the user.
 enum InAppNotification {
   /// The in-app notification for when a request fails.
   case failedResponse(DetailedResponse)
   
+  /// The `UNNotificationContent` for the in-app notification.
   var content: UNNotificationContent {
     let content = UNMutableNotificationContent()
     
