fbe355cfdb68e06868a1170e93ea70c864a91b32
martin-e91 <martin-e91@users.noreply.github.com>
2021-07-29 15:50:46 +0000

    Format code

--- Tests/AppTests/Notifications+Tests.swift
+++ Tests/AppTests/Notifications+Tests.swift
@@ -12,26 +12,28 @@ class NotificationsTests: XCTestCase {
   func testAddInAppNotification() {
     let identifier = #function
     let notification: InAppNotification = .failedResponse(statusCode: 0, path: "test")
-    
+
     Logic.Settings.Notifications.add(notification: notification, with: identifier) { error in
       XCTAssertNil(error)
     }
-    
-    UNUserNotificationCenter.current().getDeliveredNotifications { notificationRequests in
-      XCTAssertTrue(notificationRequests.contains { $0.request.identifier == identifier })
-    }
+
+    UNUserNotificationCenter.current()
+      .getDeliveredNotifications { notificationRequests in
+        XCTAssertTrue(notificationRequests.contains { $0.request.identifier == identifier })
+      }
   }
-  
+
   func testAddNotificationRequest() {
     let identifier = #function
     let content = UNNotificationContent()
-    
+
     Logic.Settings.Notifications.addNotificationRequest(with: content, identifier: identifier) { error in
       XCTAssertNil(error)
     }
-   
-    UNUserNotificationCenter.current().getDeliveredNotifications { notificationRequests in
-      XCTAssertTrue(notificationRequests.contains { $0.request.identifier == identifier })
-    }
+
+    UNUserNotificationCenter.current()
+      .getDeliveredNotifications { notificationRequests in
+        XCTAssertTrue(notificationRequests.contains { $0.request.identifier == identifier })
+      }
   }
 }
