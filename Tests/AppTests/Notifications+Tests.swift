5346d2fd9795e4663a1302b30166b6e7fe96a0b4
martin-e91 <martin-e91@users.noreply.github.com>
2021-07-29 15:38:27 +0000

    Format code

--- Tests/AppTests/Notifications+Tests.swift
+++ Tests/AppTests/Notifications+Tests.swift
@@ -2,8 +2,8 @@
 //  Mocka
 //
 
-import XCTest
 import UserNotifications
+import XCTest
 
 @testable import MockaApp
 @testable import MockaServer
@@ -12,13 +12,14 @@ class NotificationsTests: XCTestCase {
   func testAddInAppNotification() {
     let notification: InAppNotification = .failedResponse(statusCode: 0, path: "test")
     let identifier = #function
-    
+
     Logic.Settings.Notifications.add(notification: notification, with: identifier) { error in
       XCTAssertNil(error)
-      UNUserNotificationCenter.current().getDeliveredNotifications { notificationRequests in
-        XCTAssertTrue(notificationRequests.contains { $0.request.identifier == identifier })
-      }
+      UNUserNotificationCenter.current()
+        .getDeliveredNotifications { notificationRequests in
+          XCTAssertTrue(notificationRequests.contains { $0.request.identifier == identifier })
+        }
     }
   }
-  
+
 }
